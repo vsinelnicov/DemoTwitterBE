cf11476060dcf54f2f872771b6db4c65556477e3
apapuc <papucsandu@gmail.com>
2016-04-28 18:04:31 +0300

    added hash helper

--- DemoTwitter/DemoTwitter/Helpers/HashHelper.cs
+++ DemoTwitter/DemoTwitter/Helpers/HashHelper.cs
@@ -1,11 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Text;
 
 namespace DemoTwitter.Helpers
 {
     public class HashHelper
     {
+        public string CalculateMd5(string input)
+        {
+            using (System.Security.Cryptography.MD5 md5Sum = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5Sum.ComputeHash(inputBytes);
+
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    stringBuilder.Append(i.ToString("X2"));
+                }
+                return stringBuilder.ToString();
+            }
+        }
     }
 }
