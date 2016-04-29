710698b04bf8dfe417c41cb2a0c217fa8778a912
vsinelnicov <valery93s@hotmail.com>
2016-04-29 10:37:09 +0300

    Added BundleConfig Class

---
+++ DemoTwitter/DemoTwitter/App_Start/BundleConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Optimization;
+
+namespace DemoTwitter
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Content/js").Include
+                ("~/bootstrap.js",
+                "~/bootstrap.min.js",
+                "~/jquery.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include
+                ("~/Content/bootstrap.css",
+                "~/Content/bootstrap.min.css",
+                "~/Content/business-casual.css",
+                "~/Content/login.css"));
+        }
+    }
+}
--- DemoTwitter/DemoTwitter/App_Start/RouteConfig.cs
+++ DemoTwitter/DemoTwitter/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace DemoTwitter
@@ -16,7 +12,7 @@ namespace DemoTwitter
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "TweetIndexDemo", action = "TweetIndexDemo", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
