409d08782b0aff3319d1e57861997c6ff1b09a96
apapuc <papucsandu@gmail.com>
2016-05-03 12:26:05 +0300

    added redirect button login to register

--- DemoTwitter/DemoTwitter/Controllers/HomeController.cs
+++ DemoTwitter/DemoTwitter/Controllers/HomeController.cs
@@ -30,35 +30,7 @@ namespace DemoTwitter.Controllers
                 return RedirectToAction("Index", "User");
             }
             return View();
-
-
-            //    return View();
-
-            //User userFromDatabase = userRepository.GetByUsername(user.Username);
-
-            //if (userFromDatabase == null)
-            //{
-            //    return View(user);
-            //}
-            //else
-            //{
-            //    GetUserId(userFromDatabase);
-            //    Session["UserFullName"] = userFromDatabase.FirstName + " " + userFromDatabase.LastName;
-         //    return RedirectToAction("Index", "User");
-            //}
         }
-
-        //if (ModelState.IsValid)
-        //   {
-        //       user.Password = hashHelper.CalculateMd5(user.Password);
-        //       userBl.Register(user);
-        //       return View();
-        //   }
-        //   else
-        //   {
-        //       return View(user);
-        //   }
-
         public int GetUserId(User user)
         {
             int userId;
