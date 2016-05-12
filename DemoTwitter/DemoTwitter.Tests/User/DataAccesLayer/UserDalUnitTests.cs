5c3f3ca87e06bc8ac941fe7f2473c624e0d8d4c3
vsinelnicov <valery93s@hotmail.com>
2016-05-12 12:42:59 +0300

    Added Initialization method to UserDalUnitTests

--- DemoTwitter/DemoTwitter.Tests/User/DataAccesLayer/UserDalUnitTests.cs
+++ DemoTwitter/DemoTwitter.Tests/User/DataAccesLayer/UserDalUnitTests.cs
@@ -8,12 +8,13 @@ namespace DemoTwitter.Tests.User.DataAccesLayer
     public class UserDalUnitTests
     {
         IUserRepository userRepository = new UserRepository();
+        private DataAccessLayer.User validUser;
+        private DataAccessLayer.User nullUser;
 
-        [TestMethod]
-        public void Register_ValidUserAdded_ReturnsTrue()
+        [TestInitialize]
+        public void Initialization()
         {
-            //arrange
-            var user = new DataAccessLayer.User
+            validUser = new DataAccessLayer.User
             {
                 username = "ion",
                 email = "ion@ion.com",
@@ -21,9 +22,17 @@ namespace DemoTwitter.Tests.User.DataAccesLayer
                 lastname = "Popescu",
                 password = "123456"
             };
+
+            nullUser = null;
+        }
+
+        [TestMethod]
+        public void Register_ValidUserAdded_ReturnsTrue()
+        {
+            //arrange
             bool expected = true;
             //act
-            bool actual = userRepository.Register(user);
+            bool actual = userRepository.Register(validUser);
             //assert
             Assert.AreEqual(expected, actual);
         }
@@ -31,13 +40,23 @@ namespace DemoTwitter.Tests.User.DataAccesLayer
         [TestMethod]
         public void Register_NullUserAdded_ReturnsFalse()
         {
-            var user = new DataAccessLayer.User();
-            user = null;
             bool expected = false;
 
-            bool actual = userRepository.Register(user);
+            bool actual = userRepository.Register(nullUser);
 
             Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Update_NullUser_ReturnsFalse()
+        {
+
+        }
+
+        [TestCleanup]
+        public void Clear()
+        {
+
         }
     }
 }
