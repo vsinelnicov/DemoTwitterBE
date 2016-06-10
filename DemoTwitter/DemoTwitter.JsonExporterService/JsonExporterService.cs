d1ac32c09d59f886615e5703edf8a580d3c8df2e
vsinelnicov <valery93s@hotmail.com>
2016-06-10 14:21:28 +0300

    Added JSON Exporter WCF service

---
+++ DemoTwitter/DemoTwitter.JsonExporterService/JsonExporterService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Script.Serialization;
+using DemoTwitter.BusinessLayer;
+using DemoTwitter.Models;
+
+namespace DemoTwitter.JsonExporterService
+{
+
+
+    public class JsonExporterService : IJsonExporterService
+    {
+
+        private readonly ITweetBL tweetBL;
+
+        public JsonExporterService(ITweetBL tweetBL)
+        {
+            this.tweetBL = tweetBL;
+        }
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public string GetAllTweets()
+        {
+            var allTweets = tweetBL.GetAll();
+            string output = new JavaScriptSerializer().Serialize(allTweets);
+            return output;
+        }
+
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
